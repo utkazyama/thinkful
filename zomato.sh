e5ca9b742a57e18fff51c212034ebcb95a834efa
github username <github email address>
2020-05-29 17:44:11 -0500

    done

--- zomato.sh
+++ zomato.sh
@@ -1,6 +1,6 @@
-for i in {0..80..20}
+for i in {200..280..20}
 do
 	BEG=$i
 	END=$(($i+20))
-	curl -X GET --header "Accept: application/json" --header "user-key: d9fd16b5f1a2bf1d8e26fab89b6c3388" "https://developers.zomato.com/api/v2.1/search?entity_id=292&entity_type=city&start=$BEG&count=$END" -o file$BEG.json
+	curl -X GET --header "Accept: application/json" --header "user-key: e78e739b15a8e5d55c6c4bb69f3f40b6" "https://developers.zomato.com/api/v2.1/search?entity_id=292&entity_type=city&start=$BEG&count=$END" -o file$BEG.json
 done
